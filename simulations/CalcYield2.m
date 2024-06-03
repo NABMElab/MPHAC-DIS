d91ea80cdc4e7c2d1b374407b59683fbb51fb22e
NABMElab <nabmelab@126.com>
2024-06-03 09:58:09 +0800

    Delete simulations/CalcYield2.m

--- simulations/CalcYield2.m
+++
@@ -1,9 +0,0 @@
-function [yield] = CalcYield2(dG, Temp, conc, conc2)
-Keq = dG2Keq(dG, Temp);
-if min(roots([Keq-1, -Keq*(conc+conc2), Keq*conc*conc2]))>0
-yield = min(roots([Keq-1, -Keq*(conc+conc2), Keq*conc*conc2]))/min(conc, conc2);
-else
-yield = max(roots([Keq-1, -Keq*(conc+conc2), Keq*conc*conc2]))/min(conc, conc2);
-end
-end
-% calculate yield of strand displacement (A + BC → AB + C)
